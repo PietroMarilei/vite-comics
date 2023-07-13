0921c1835d39c0514764f57a814e5f3f0e5767bf
PietroMarilei~ <pietromrl0@gmail.com>
2023-07-13 18:17:19 +0200

    add footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -36,6 +36,81 @@ export default {
          <p>Digital Comics</p>
       </div>
     </div>
+    <div class="my_jumbo_footer">
+
+      <div class="my_footer_left">
+        <div class="col-4 my_comics">
+          <h3>DC COMICS</h3>
+          <ul>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+          </ul>
+          <div class="my_shop">
+            <h3>MY SHOP</h3>
+            <ul>
+              <li>Shop DC</li>
+              <li>Shop DC Collectibles</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-4 my_DC">
+          <h3>DC </h3>
+          <ul>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+          </ul>
+        </div>
+        <div class="col-4 my_sites">
+          <h3>SITES</h3>
+          <ul>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+            <li>Characters</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="my_footer_right">
+        <img src="../assets/img/dc-logo-bg.png" alt="">
+      </div>
+    </div>
+    <div class="my_footer_footer">
+      <button>
+        <a href="#">
+          SIGN UP NOW
+        </a>
+      </button>
+
+      <div class="my_right_ff">
+        <h3>FOLLOW US</h3>
+        <a href="">
+          <img src="../assets/img/footer-facebook.png" alt="">
+        </a>
+        <a href="">
+          <img src="../assets/img/footer-twitter.png" alt="">
+        </a>
+        <a href="">
+          <img src="../assets/img/footer-youtube.png" alt="">
+        </a>
+        <a href="">
+          <img src="../assets/img/footer-pinterest.png" alt="">
+        </a>
+        <a href="">
+          <img src="../assets/img/footer-periscope.png" alt="">
+        </a>
+      </div>
+    </div>
   </footer>
 
 </template>
@@ -49,13 +124,11 @@ export default {
       align-items: center;
       padding: 2rem;
       background-color:$blue_banner ;
-      font-stretch: condensed;
+      font-stretch: semi-condensed;
       text-transform: uppercase;
-    };
-    #gps_logo {
-      width: 40px;
-    };
-    .my_item {
+      font-weight: 600;
+
+      .my_item {
       display: flex;
       align-items: center;
       color: $secondary-color-white;
@@ -68,6 +141,80 @@ export default {
 
       p {
         padding: 0.5rem;
+      }
+    }
+    };
+    #gps_logo {
+      width: 40px;
+    };
+    
+    .my_jumbo_footer {
+      background-image: url(../assets/img/footer-bg.jpg);
+      object-fit: cover;
+      padding: 0 8rem;
+      height: 20rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .my_footer_left {
+        color: white;
+        display: flex;
+        justify-content: space-between;
+        .col-4 {
+          width: 8rem;
+          margin: 0 auto;
+        }
+        h3 {
+          font-weight: 700;
+          padding: 1rem 0;
+        }
+        li {
+          color: rgb(166, 166, 166);
+        }
+      }
+      }
+
+      .my_footer_right {
+        img {
+          height: 20rem;
+          object-fit: cover;
+          width: 500px;
+          
+        }
+        
+    }
+
+    .my_footer_footer {
+      background-color: $dark_grey;
+      height: 6rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 8rem;
+
+      button {
+        font-weight: 800;
+        font-stretch: condensed;
+        color: $secondary-color-white;
+        border: 2px solid $blue-banner;
+        border-radius: 5px;
+        padding: 0.8rem;
+        cursor: pointer;
+      }
+      .my_right_ff {
+        display: flex;
+        align-items: center;
+
+        h3 {
+          color: $blue-banner;
+          font-weight: 800;
+          font-stretch: condensed;
+          padding: 0 1rem;
+        }
+        a {
+          padding: 0 0.3rem;
+        }
       }
     }
 </style>

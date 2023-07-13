c87679940eb7ca280e6d711aad66ac446d8702dd
Pietro Marilei <119878978+PietroMarilei@users.noreply.github.com>
2023-07-13 16:10:20 +0200

    Initial commit

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<template>
+    <!-- main template -->
+    <main>
+    MAIN
+  </main>
+
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
